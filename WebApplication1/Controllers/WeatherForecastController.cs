e64f8072d8c24fb2ac89bd9f2827a106bffa0733
Gerson Abreu <125635864+GersonAbreu17@users.noreply.github.com>
2026-03-18 19:58:01 -0300

    Add files via upload

---
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("/")]
+        public String Get()
+        {
+            return "Hello World!";
+        }
+    }
+}
